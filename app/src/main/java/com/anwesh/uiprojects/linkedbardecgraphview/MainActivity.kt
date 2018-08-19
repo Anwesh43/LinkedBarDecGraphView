93326705543ac58e8f4ec19e3cda6222071145c0
Anwesh43 <anweshthecool0@gmail.com>
2018-08-20 02:13:46 +0530

    adding BarDecGraphView in Activity

--- app/src/main/java/com/anwesh/uiprojects/linkedbardecgraphview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedbardecgraphview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedbardecgraphview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.bardecgraphview.BarDecGraphView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        BarDecGraphView.create(this)
     }
 }
