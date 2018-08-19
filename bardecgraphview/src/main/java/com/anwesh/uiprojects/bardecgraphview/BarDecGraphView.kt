e7311be964a873acd5179fb0590bb20ec1207f1b
Anwesh43 <anweshthecool0@gmail.com>
2018-08-20 01:17:04 +0530

    defining node number and importing libraries

--- bardecgraphview/src/main/java/com/anwesh/uiprojects/bardecgraphview/BarDecGraphView.kt
+++ bardecgraphview/src/main/java/com/anwesh/uiprojects/bardecgraphview/BarDecGraphView.kt
@@ -3,3 +3,13 @@ package com.anwesh.uiprojects.bardecgraphview
 /**
  * Created by anweshmishra on 20/08/18.
  */
+
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.RectF
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+
+val nodes : Int = 5
+
